b6df790e28c135ef9572b11bf71c2daf8c5e0537
张云飞 <zhangyunfei@okjiaoyu.cn>
2017-09-29 17:14:28 +0800

    add error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 | 自得其乐</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="description" content="自得其乐，字得其乐，传统文化，字典，甲骨文，道德经，道家，诸子百家，古诗词曲 | 自得其乐">
+    <meta name="keywords" content="自得其乐，字得其乐，传统文化，字典，甲骨文，道德经，道家，诸子百家，古诗词曲 | 自得其乐">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center">
+            <h3>你访问的页面不存在了...</h3>
+            <div class="row">
+                <div class="col-md-7 text-center">
+                    <img src="{{ asset('/images/404.jpg') }}" alt="404" class="img-responsive img-rounded">
+                </div>
+                <div class="col-md-5">
+                    <div class="text-left" style="margin-top:60px">
+                        <blockquote>
+                            <h5>南阜小亭台，薄有山花取次开</h5>
+                            <h5>寄语多情熊少府</h5>
+                            <h5>晴也须来，雨也须来</h5>
+                            <h5>随意且衔杯，莫惜春衣坐绿苔</h5>
+                            <h5>若待明朝风雨过</h5>
+                            <h5>人在天涯</h5>
+                            <h5>春在天涯</h5>
+                            <h5>--- 《南乡一剪梅》 虞集</h5>
+                        </blockquote>
+                    </div>
+                    <a href="http://www.zzideqile.com">返回首页</a>
+                    <br/><br/><br/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/front/zhuanti.blade.php
+++ resources/views/front/zhuanti.blade.php
@@ -6,20 +6,27 @@
 	<div class="main main-raised">
 		<div class="container">
 			<h2 class="title">专题列表</h2>
-
-			<ul class="list">
-				@foreach($zhuantis as $zhuanti)
-					<li class="list-group-item">
-                        <a href="{{ route('zhuanti-info', $zhuanti->id) }}"> {{ $zhuanti->name }} </a>
-                    </li>
-				@endforeach
-			</ul>
-
+			<div class="table-responsive">
+				<table class="table table-bordered table-hover">
+					<tr>
+						<th class="text-center">#</th>
+						<th>标题</th>
+					</tr>
+					<tbody>
+					@foreach($zhuantis as $k=>$zhuanti)
+						<tr>
+							<td class="text-center">{{ $k+1 }}</td>
+							<td>
+								<a href="{{ route('zhuanti-info', $zhuanti->id) }}"> {{ $zhuanti->name }} </a>
+							</td>
+						</tr>
+					@endforeach
+					</tbody>
+				</table>
+			</div>
             {!! $zhuantis->links() !!}
-
 		</div>
 	</div>
-
 @endsection
 
 @section('js')
